f2ccab6dd4811c012022929f16fdd453b5cbcfa3
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-01 22:24:15 +1100

    fix up docstring indentation

--- test/aoc/day1_test.exs
+++ test/aoc/day1_test.exs
@@ -13,28 +13,28 @@ defmodule AOC.Day1Test do
   describe "part1" do
     test "provided data 1" do
       data = """
-+1
-+1
-+1
-"""
+      +1
+      +1
+      +1
+      """
       assert AOC.Day1.part1(data |> stream) == 3
     end
 
     test "provided data 2" do
       data = """
-+1
-+1
--2
-"""
+      +1
+      +1
+      -2
+      """
       assert AOC.Day1.part1(data |> stream) == 0
     end
 
     test "provided data 3" do
       data = """
--1
--2
--3
-"""
+      -1
+      -2
+      -3
+      """
       assert AOC.Day1.part1(data |> stream) == -6
     end
   end
@@ -42,42 +42,42 @@ defmodule AOC.Day1Test do
   describe "part2" do
     test "provided data 1" do
       data = """
-+1
--1
-"""
+      +1
+      -1
+      """
       assert AOC.Day1.part2(data |> stream) == 0
     end
 
     test "provided data 2" do
       data = """
-+3
-+3
-+4
--2
--4
-"""
+      +3
+      +3
+      +4
+      -2
+      -4
+      """
       assert AOC.Day1.part2(data |> stream) == 10
     end
 
     test "provided data 3" do
       data = """
--6
-+3
-+8
-+5
--6
-"""
+      -6
+      +3
+      +8
+      +5
+      -6
+      """
       assert AOC.Day1.part2(data |> stream) == 5
     end
 
     test "provided data 4" do
       data = """
-+7
-+7
--2
--7
--4
-"""
+      +7
+      +7
+      -2
+      -7
+      -4
+      """
       assert AOC.Day1.part2(data |> stream) == 14
     end
   end
