c47352e3bc0f2b715da4e0b0d2c551cdafb9119e
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-05 22:49:17 +1100

    split components into own function

--- lib/aoc/day2.ex
+++ lib/aoc/day2.ex
@@ -1,10 +1,8 @@
 defmodule AOC.Day2 do
   def part1(data_stream) do
-    result =
-      data_stream
-      |> Stream.map(&letter_count_set/1)
-      |> Enum.reduce(%{}, fn lcs, totals -> lcs |> count_values(totals) end)
-    result[2] * result[3]
+    with components <- checksum_components(data_stream) do
+      components[2] * components[3]
+    end
   end
 
   def letter_count_set(str) do
@@ -13,6 +11,12 @@ defmodule AOC.Day2 do
     |> count_values()
     |> Map.values()
     |> MapSet.new()
+  end
+
+  defp checksum_components(data_stream) do
+    data_stream
+    |> Stream.map(&letter_count_set/1)
+    |> Enum.reduce(%{}, fn lcs, totals -> lcs |> count_values(totals) end)
   end
 
   defp count_values(map, initial_acc \\ %{}) do
