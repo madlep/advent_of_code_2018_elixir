4e85a678d45c308c15b8bad0b5fef9c41917a3fb
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-05 22:41:56 +1100

    extracted common list value adding function

--- lib/aoc/day2.ex
+++ lib/aoc/day2.ex
@@ -3,21 +3,22 @@ defmodule AOC.Day2 do
     result =
       data_stream
       |> Stream.map(&letter_count_set/1)
-      |> Enum.reduce(%{}, fn lcs, totals ->
-        lcs
-        |> Enum.reduce(totals, fn n, lcs_totals ->
-          Map.update(lcs_totals, n, 1, &(&1 + 1))
-        end)
-      end)
-
+      |> Enum.reduce(%{}, fn lcs, totals -> lcs |> count_values(totals) end)
     result[2] * result[3]
   end
 
   def letter_count_set(str) do
     str
     |> String.graphemes()
-    |> Enum.reduce(%{}, fn c, acc -> Map.update(acc, c, 1, &(&1 + 1)) end)
+    |> count_values()
     |> Map.values()
     |> MapSet.new()
+  end
+
+  defp count_values(map, initial_acc \\ %{}) do
+    map
+    |> Enum.reduce(initial_acc, fn key, counts ->
+      Map.update(counts, key, 1, &(&1 + 1))
+    end)
   end
 end
