26f7b8a484d648f18f07c9d1f47487d52c74a03a
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-10 00:11:46 +1100

    implemented parser combinator

---
+++ lib/aoc/day3.ex
@@ -0,0 +1,33 @@
+defmodule AOC.Day3 do
+  defmodule Parser do
+    import NimbleParsec
+
+    whitespace = ignore(string(" "))
+
+    id =
+      ignore(string("#"))
+      |> integer(min: 1)
+
+    co_ords =
+      unwrap_and_tag(integer(min: 1), :x)
+      |> ignore(string(","))
+      |> unwrap_and_tag(integer(min: 1), :y)
+
+    dimensions =
+      unwrap_and_tag(integer(min: 1), :width)
+      |> ignore(string("x"))
+      |> unwrap_and_tag(integer(min: 1), :height)
+
+    # #123 @ 3,2: 5x4
+    defparsec(
+      :claim,
+      unwrap_and_tag(id, :id)
+      |> concat(whitespace)
+      |> ignore(string("@"))
+      |> concat(whitespace)
+      |> concat(co_ords)
+      |> ignore(string(": "))
+      |> concat(dimensions)
+    )
+  end
+end
