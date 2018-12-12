d35ec5da76d8d8823cdaee4f6364986871c026d1
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-13 00:02:22 +1100

    part 2 test

--- test/aoc/day3_test.exs
+++ test/aoc/day3_test.exs
@@ -15,6 +15,18 @@ defmodule AOC.Day3Test do
     end
   end
 
+  describe "part 2" do
+    test "handles provided example" do
+      data = """
+      #1 @ 1,3: 4x4
+      #2 @ 3,1: 4x4
+      #3 @ 5,5: 2x2
+      """
+
+      assert AOC.Day3.part2(data |> stream) == 3
+    end
+  end
+
   defmodule ParserTest do
     use ExUnit.Case
 
