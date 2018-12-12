21e988a4a21d5c2166c46b5ef4f3b8ac401b7225
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-12 15:53:20 +1100

    extracted area to own file

--- lib/aoc/day3.ex
+++ lib/aoc/day3.ex
@@ -1,48 +1,5 @@
 defmodule AOC.Day3 do
   end
 
-  defmodule Area do
-    use Bitwise
-
-    @enforce_keys [:data, :row_size]
-    defstruct data: 0, row_size: 0
-
-    @default_row_size 1000
-
-    def new(row_size \\ @default_row_size, x: x, y: y, width: width, height: height) do
-      data =
-        1..height
-        |> Enum.reduce(0, fn row, acc ->
-          1..width
-          |> Enum.reduce(acc, fn col, acc2 ->
-            cell = col + x - 1 + (row + y - 1) * row_size
-            acc2 + (1 <<< cell)
-          end)
-        end)
-
-      %Area{data: data, row_size: row_size}
-    end
-
-    def count(%Area{data: data}), do: do_count(data, 0)
-
-    defp do_count(0, count), do: count
-
-    defp do_count(data, count), do: do_count(data >>> 1, count + (data &&& 1))
-
-    def intersection(
-          %Area{data: data1, row_size: row_size},
-          %Area{data: data2, row_size: row_size}
-        ),
-        do: %Area{row_size: row_size, data: data1 &&& data2}
-
-    def intersection(%Area{row_size: _r1}, %Area{row_size: _r2}), do: {:error, :row_size_mismatch}
-
-    def union(
-          %Area{data: data1, row_size: row_size},
-          %Area{data: data2, row_size: row_size}
-        ),
-        do: %Area{row_size: row_size, data: data1 ||| data2}
-
-    def union(%Area{row_size: _r1}, %Area{row_size: _r2}), do: {:error, :row_size_mismatch}
   end
 end
---
+++ lib/aoc/day3/area.ex
@@ -0,0 +1,64 @@
+defmodule AOC.Day3.Area do
+  use Bitwise
+
+  alias AOC.Day3.Area
+
+  @enforce_keys [:data, :row_size]
+  defstruct data: 0, row_size: 0
+
+  @default_row_size 1000
+
+  def empty(row_size \\ @default_row_size), do: %Area{data: 0, row_size: row_size}
+
+  def new(row_size \\ @default_row_size, x: x, y: y, width: width, height: height) do
+    data =
+      1..height
+      |> Enum.reduce(0, fn row, acc ->
+        cell_y = (row + y - 1) * row_size
+        from = x + cell_y
+        to = x + width + cell_y
+        acc + (1 <<< to) - (1 <<< from)
+      end)
+
+    %Area{data: data, row_size: row_size}
+  end
+
+  def count(%Area{data: data}), do: do_count(data, 0)
+
+  @m1  0x5555555555555555
+  @m2  0x3333333333333333
+  @m4  0x0f0f0f0f0f0f0f0f
+  @m8  0x00ff00ff00ff00ff
+  @m16 0x0000ffff0000ffff
+  @m32 0x00000000ffffffff
+
+  defp do_count(0, count), do: count
+
+  defp do_count(data, count) do
+    x = data
+    x = (x &&& @m1) + ((x >>> 1) &&& @m1)
+    x = (x &&& @m2) + ((x >>> 2) &&& @m2)
+    x = (x &&& @m4) + ((x >>> 4) &&& @m4)
+    x = (x &&& @m8) + ((x >>> 8) &&& @m8)
+    x = (x &&& @m16) + ((x >>> 16) &&& @m16)
+    x = (x &&& @m32) + ((x >>> 32) &&& @m32)
+    data = data >>> 64
+    do_count(data, count + x)
+  end
+
+  def intersection(
+    %Area{data: data1, row_size: row_size},
+    %Area{data: data2, row_size: row_size}
+  ),
+  do: %Area{row_size: row_size, data: data1 &&& data2}
+
+  def intersection(%Area{row_size: _r1}, %Area{row_size: _r2}), do: {:error, :row_size_mismatch}
+
+  def union(
+    %Area{data: data1, row_size: row_size},
+    %Area{data: data2, row_size: row_size}
+  ),
+  do: %Area{row_size: row_size, data: data1 ||| data2}
+
+  def union(%Area{row_size: _r1}, %Area{row_size: _r2}), do: {:error, :row_size_mismatch}
+end
