cced6443a98cdd30deadbaed4e1f02281a6986ec
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-12 15:54:04 +1100

    test for part 1

--- test/aoc/day3_test.exs
+++ test/aoc/day3_test.exs
@@ -1,6 +1,20 @@
 defmodule AOC.Day3Test do
   use ExUnit.Case
 
+  import AOC.StringStream
+
+  describe "part 1" do
+    test "handles provided example" do
+      data = """
+      #1 @ 1,3: 4x4
+      #2 @ 3,1: 4x4
+      #3 @ 5,5: 2x2
+      """
+
+      assert AOC.Day3.part1(data |> stream) == 4
+    end
+  end
+
   defmodule ParserTest do
     use ExUnit.Case
 
