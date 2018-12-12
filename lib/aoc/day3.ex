fa2e2d1f75fbff4f8747799003dcdf7531b2b80e
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-12 23:58:12 +1100

    working part 2

--- lib/aoc/day3.ex
+++ lib/aoc/day3.ex
@@ -12,8 +12,14 @@ defmodule AOC.Day3 do
     Area.count(result.overlaps)
   end
 
+  def part2(data) do
+    result = run(data)
+    good_claim = result.good_claims |> hd()
+    good_claim.id
+  end
+
   defmodule State do
-    defstruct claimed: Area.empty(), overlaps: Area.empty()
+    defstruct claimed: Area.empty(), overlaps: Area.empty(), good_claims: []
   end
 
   defp run(data) do
@@ -25,10 +31,19 @@ defmodule AOC.Day3 do
 
       Area.new(d([id, x, y, width, height]))
     end)
-    |> Enum.reduce(%State{}, fn {:ok, new_claim}, %State{claimed: claimed, overlaps: overlaps} ->
+    |> Enum.reduce(%State{}, fn {:ok, new_claim}, d(%State{claimed, overlaps, good_claims}) ->
       new_claim_overlap = Area.intersection(claimed, new_claim)
 
+      good_claims =
+        if Area.empty?(new_claim_overlap) do
+          [new_claim | good_claims]
+        else
+          good_claims
+          |> Enum.reject(fn (claim) -> !Area.empty?(Area.intersection(claim, new_claim)) end)
+        end
+
       %State{
+        good_claims: good_claims,
         claimed: Area.union(claimed, new_claim),
         overlaps: Area.union(overlaps, new_claim_overlap)
       }
