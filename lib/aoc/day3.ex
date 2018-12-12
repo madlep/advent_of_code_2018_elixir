2740873ddc9dac16d84499b94f76e2b2cc39153f
Julian Doherty (madlep) <madlep@madlep.com>
2018-12-12 15:53:38 +1100

    implement part 1

--- lib/aoc/day3.ex
+++ lib/aoc/day3.ex
@@ -1,5 +1,32 @@
 defmodule AOC.Day3 do
+  alias AOC.Day3.{
+    Parser,
+    State,
+    Area
+  }
+
+  defmodule State do
+    defstruct claimed: Area.empty(), overlaps: Area.empty()
   end
 
+  def part1(data) do
+    data
+    |> Stream.map(fn line ->
+      {:ok, tokens, _rest, _context, _line, _byte_offset} = Parser.claim(line)
+      tokens
+    end)
+    |> Enum.reduce(%State{}, fn [id: _id, x: x, y: y, width: w, height: h],
+                                %State{claimed: claimed, overlaps: overlaps} ->
+      new_claim = Area.new(x: x, y: y, width: w, height: h)
+      new_claim_overlap = Area.intersection(claimed, new_claim)
+
+      %State{
+        claimed: Area.union(claimed, new_claim),
+        overlaps: Area.union(overlaps, new_claim_overlap)
+      }
+    end)
+    |> Map.get(:overlaps)
+    |> Area.count()
   end
+
 end
